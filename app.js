c41d36167d0d59f45e1dc794e64882105b85f178
AthulCS <athulcs.cs@gmail.com>
2017-11-12 15:59:29 +0530

    node home

--- app.js
+++ app.js
@@ -5,9 +5,20 @@ var connection = mysql.createConnection({
 
   host     : 'localhost',
   user     : 'root',
-  password : '',
+  password : 'root',
   database : 'mydb'
 });
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.listen(3000,function(){
+    console.log('Node server running @ http://localhost:3000')
+});
+app.use('/node_modules',  express.static(__dirname + '/node_modules'));
+
+app.use('/style',  express.static(__dirname + '/style'));
+
+app.get('/',function(req,res){
+    res.sendFile('index.html',{'root': __dirname + '/templates'});
+
+});
